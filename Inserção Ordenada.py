975b2054fe2238208fcdd2a35449276b0f099aaf
Fabio Zanon <fabioonoue7@gmail.com>
2026-01-11 18:17:32 -0300

    Update Inserção Ordenada.py

--- Inserção Ordenada.py
+++ Inserção Ordenada.py
@@ -6,7 +6,8 @@ for valores in range(0, len(lista)):
     if novo_numero <= lista[valores]:
         lista.insert(valores, novo_numero)
         break
-    if novo_numero > lista[valores]:
-        lista.append(novo_numero)
+if novo_numero > lista[valores]:
+    lista.append(novo_numero)
 
 print(lista)
+
